ab54b4e44a3228c2395ae1bb20d76075806070bd
gorlitzer <gorlitzerpark@gmail.com>
2024-04-01 12:36:11 +0200

    restore change

--- picamera2/main.py
+++ picamera2/main.py
@@ -57,9 +57,7 @@ def configDNN():
 (dnn, classNames) = configDNN()
 
 picam2 = Picamera2()
-config = picam2.configure(
-    picam2.create_video_configuration(main={"size": (1920, 1080), "format": "RGB888"})
-)
+config = picam2.create_preview_configuration({"format": "RGB888"})
 picam2.configure(config)
 picam2.start()
 
