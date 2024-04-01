10935999aac32f623d66471d26e6edc4a19c556e
gorlitzer <gorlitzerpark@gmail.com>
2024-04-01 21:57:26 +0200

    print only recognized array

--- picamera2/main.py
+++ picamera2/main.py
@@ -53,10 +53,10 @@ class VideoStreamHandler(BaseHTTPRequestHandler):
 
         while True:
             pc2array = picam2.capture_array()
-            image, recognized_objs = objectRecognition(
+            image, recognizedArray = objectRecognition(
                 dnn, classNames, pc2array, 0.6, 0.6
             )
-            print(image, recognized_objs)
+            print(recognizedArray)
             # Check if a person is detected
             # if "person" in result:
             #     if not recording:
