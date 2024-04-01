2a80026af4d9506a948c7853157076393bb6e1b7
gorlitzer <gorlitzerpark@gmail.com>
2024-04-01 11:26:48 +0200

    add picamera2

--- opencv/setup.sh
+++ opencv/setup.sh
@@ -4,14 +4,14 @@
 if [ ! -f "MobileNetSSD_deploy.prototxt" ]; then
     echo "Downloading MobileNetSSD model file definition..."
     url="https://raw.githubusercontent.com/djmv/MobilNet_SSD_opencv/master/MobileNetSSD_deploy.prototxt"
-    wget -O ./models/MobileNetSSD/MobileNetSSD_deploy.prototxt "$url"
+    wget -O ./opencv/models/MobileNetSSD/MobileNetSSD_deploy.prototxt "$url"
 fi
 
 # Download MobileNetSSD Pre-trained model weights
 if [ ! -f "MobileNetSSD_deploy.caffemodel" ]; then
     echo "Downloading MobileNetSSD pre-trained model weights..."
     url="https://raw.githubusercontent.com/djmv/MobilNet_SSD_opencv/master/MobileNetSSD_deploy.caffemodel"
-    wget -O ./models/MobileNetSSD/MobileNetSSD_deploy.caffemodel "$url"
+    wget -O ./opencv/models/MobileNetSSD/MobileNetSSD_deploy.caffemodel "$url"
 fi
 
 
---
+++ picamera2/visualize_detections.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cv2
+
+
+def log_detection(detections, classNames):
+    for i in range(detections.shape[2]):
+        # Confidence of prediction
+        confidence = detections[0, 0, i, 2]
+
+        # Set confidence level threshold to filter weak predictions
+        if confidence > 0.75:
+            # Get class id
+            class_id = int(detections[0, 0, i, 1])
+
+            # Create label
+            label_val = "%.3f" % (confidence)  # Round off to 3 decimal places
+            label = classNames[class_id] + ": " + str(label_val)
+
+            # Log to terminal
+            print("Detected:", label)
+
+
+def visualize_detection(frame, detections, classNames, color_map):
+    for i in range(detections.shape[2]):
+        # Confidence of prediction
+        confidence = detections[0, 0, i, 2]
+
+        # Set confidence level threshold to filter weak predictions
+        if confidence > 0.75:
+            # Get class id
+            class_id = int(detections[0, 0, i, 1])
+
+            # Scale to the frame
+            width = frame.shape[1]
+            height = frame.shape[0]
+            x_top_left = int(detections[0, 0, i, 3] * width)
+            y_top_left = int(detections[0, 0, i, 4] * height)
+            x_bottom_right = int(detections[0, 0, i, 5] * width)
+            y_bottom_right = int(detections[0, 0, i, 6] * height)
+
+            # Draw bounding box around the detected object with the corresponding color
+            color = color_map.get(
+                classNames[class_id], (0, 0, 0)
+            )  # Default to black if class color not found
+
+            cv2.rectangle(
+                frame,
+                (x_top_left, y_top_left),
+                (x_bottom_right, y_bottom_right),
+                color,
+                thickness=2,
+            )
+
+            # Create label
+            label_val = "%.3f" % (confidence)  # Round off to 3 decimal places
+            label = classNames[class_id] + ": " + str(label_val)
+
+            # Get width and height of the label string
+            (w, h), t = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+
+            # Draw bounding box around the text
+            cv2.rectangle(
+                frame,
+                (x_top_left, y_top_left - h),
+                (x_top_left + w, y_top_left + t),
+                color,
+                cv2.FILLED,
+            )
+
+            # Draw label text
+            cv2.putText(
+                frame,
+                label,
+                (x_top_left, y_top_left),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                thickness=2,
+            )
