c38ac7a0c34163bc74106a43d4e31c9cee690f5b
gorlitzer <gorlitzerpark@gmail.com>
2024-04-01 12:40:16 +0200

    remove raw

--- picamera2/main.py
+++ picamera2/main.py
@@ -57,9 +57,7 @@ def configDNN():
 (dnn, classNames) = configDNN()
 
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(
-    raw={"format": "RGB", "size": (1920, 1080)}
-)
+config = picam2.create_preview_configuration({"format": "RGB", "size": (1920, 1080)})
 picam2.configure(config)
 picam2.start()
 
