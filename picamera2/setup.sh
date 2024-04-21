d901139424dfafdb22777750d9fc29d0e7c432b8
gorlitzer <gorlitzerpark@gmail.com>
2024-04-21 09:19:31 +0200

    better naming

--- picamera2/setup.sh
+++ picamera2/setup.sh
@@ -3,18 +3,13 @@
 # Define the URL to download the zip file
 url="https://core-electronics.com.au/media/kbase/491/Object_Detection_Files.zip"
 
-# Define the directory where you want to extract the contents
-directory="./Object_Detection_Files"
-
-# Create the directory if it doesn't exist
-mkdir -p "$directory"
 
 # Download the zip file
-wget -O "$directory/Object_Detection_Files.zip" "$url"
+wget -O "Object_Detection_Files.zip" "$url"
 
 # Extract the contents of the zip file into the specified directory
-unzip "$directory/Object_Detection_Files.zip" -d "$directory"
+unzip "Object_Detection_Files.zip" -d "$directory"
 
 # Remove the zip file after extraction
-rm "$directory/Object_Detection_Files.zip"
+rm "Object_Detection_Files.zip"
 
